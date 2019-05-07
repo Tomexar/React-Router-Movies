c8b149fa4e2d1c7ff32d59c1b8a5f0bed14b1733
Tomexar <tommyexar@gmail.com>
2019-05-07 16:06:50 -0600

    got save feature to work, trying to not duplicate saved movies

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
+
 import Movie from './Movies/Movie';
 import {Route} from 'react-router-dom';
 
@@ -24,7 +25,9 @@ export default class App extends Component {
         <SavedList list={this.state.savedList} />
         <div>
           <Route exact path = '/' component = {MovieList}/>
-          <Route exact path = '/movies/:id'  component = {Movie}/>
+          <Route path="/movies/:id" render={ (props) => {
+          return(<Movie {...props} addToSavedList={this.addToSavedList} savedList = {this.state.savedList}/>)
+        }} />
 
         </div>
       </div>
